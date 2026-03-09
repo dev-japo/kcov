f1a3465114bb0bd4adbecac204dcd28ab7b72cc4
Jan Polensky <japo@linux.ibm.com>
2026-03-09 18:45:32 +0100

    cmake: Add s390/s390x architecture detection
    Add support for detecting s390 (31-bit) and s390x (64-bit) architectures
    in the CMake build system. This is the first step in enabling kcov
    support for IBM System z / IBM Z mainframe systems.
    
    The detection uses standard preprocessor macros __s390__ and __s390x__
    to identify the target architecture.

--- cmake/TargetArch.cmake
+++ cmake/TargetArch.cmake
@@ -42,6 +42,10 @@ set(archdetect_c_code "
     #else
         #error cmake_ARCH ppc
     #endif
+#elif defined(__s390x__)
+    #error cmake_ARCH s390x
+#elif defined(__s390__)
+    #error cmake_ARCH s390
 #endif
 
 #error cmake_ARCH unknown
